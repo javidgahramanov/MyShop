0a50765f4949c3a44bb74b3a2c026de0db9284b0
XTreme.ws <XTreme.ws@DESKTOP-A2H9FU9>
2019-10-23 08:08:51 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductsComponent,
+    HomeComponent,
+    MyOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+     {path: '', component: HomeComponent},
+     {path: 'products', component: ProductsComponent},
+     {path: 'my-orders', component: MyOrdersComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
